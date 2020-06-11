6c7358abaad66ddbf76582bbc9bf7f210674ec09
Daniela Esparza <daesparz@uc.cl>
2020-06-10 22:15:09 -0700

    fixing indentation

--- queries/Cohort_A.sql
+++ queries/Cohort_A.sql
@@ -1,4 +1,4 @@
 select customer_id
-		from cohort_orders
+	from cohort_orders
 	where cast(order_date as date) > cast('2017-02-28' as date) - '30 days'::interval
-	  	and order_number = 1;
+	and order_number = 1;
--- queries/Cohort_B_2.sql
+++ queries/Cohort_B_2.sql
@@ -1,15 +1,15 @@
 -- Solution 2: Filtering users from cohort A using outer join
 with cohort_a as 
 (select customer_id
-		from cohort_orders
+	from cohort_orders
 	where cast(order_date as date) > cast('2017-02-28' as date) - '30 days'::interval
-	  	and order_number = 1)
+	and order_number = 1)
 	  	
 
 select customer_id
-		from 	  cohort_orders 
-		left join cohort_a using(customer_id)
+	from cohort_orders 
+	left join cohort_a using(customer_id)
 	where cohort_a.customer_id is null
-		and cast(order_date as date) > cast('2017-02-28' as date) - '60 days'::interval
-	  	and order_number = 1
-	order by customer_id;
+	and cast(order_date as date) > cast('2017-02-28' as date) - '60 days'::interval
+	and order_number = 1
+order by customer_id;
